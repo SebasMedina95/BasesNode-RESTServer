c3277f71055e353ed13842ac2c6977add2bf92df
Juan Sebastian Medina Toro <JSebastian19952011@gmail.com>
2023-06-20 10:18:35 -0500

    Se ajusta el validar-archivo.js donde le anexamos la validación de los tamaños de archivo, solo permitirémos subir archivos de 2MB máximo

--- middlewares/validar-archivo.js
+++ middlewares/validar-archivo.js
@@ -9,6 +9,14 @@ const validarArchivoSubida = (req = request , res = response, next) => {
         });
     }
 
+    //Validemos el tamaño de una vez, las extensiones se validan por aparte pero siempre
+    //vamos a permitir archivos de máximo 2MB -> 2000000Byte
+    if(req.files.archivo.size > 2000000){
+        return res.status(400).json({
+            msg: "El archivo subido no debe exceder los 2MB de peso ... Validación de Archivo"
+        });
+    }
+
     next();
 
 }
